97458bbf7a33be905246043137233bdf04254d44
+ <+>
2021-08-26 10:10:00 -0400

    conditonal rendering of buttons, display user info on profile page

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,13 @@ import LogoutButton from './LogoutButton';
 import Profile from './Profile';
 
 import Router from './Router'
+import { useAuth0 } from '@auth0/auth0-react';
 
 const App = () => {
+    const { isLoading } = useAuth0()
+
+    if (isLoading) return <div>Loading...</div>
+
     return(
         <div>
             <LoginButton />
