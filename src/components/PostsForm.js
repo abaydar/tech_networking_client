706827ed38ab6309b363e50d9f84d677380048ec
+ <+>
2021-09-02 15:54:15 -0400

    style usercard and postform

--- src/components/PostsForm.js
+++ src/components/PostsForm.js
@@ -24,12 +24,12 @@ class PostsForm extends Component {
 
     render(){
         return(
-            <form onSubmit={this.handleSubmit}>
-                <label>Title: </label>
-                <input type="text" value={this.state.title} onChange={this.handleChange} name="title" />
-                <label>Content: </label>
-                <textarea value={this.state.content} onChange={this.handleChange} name="content" />
-                <input type="submit" value="Publish"/>
+            <form class="p-8 bg-purple-200 rounded-xl shadow-md flex items-center space-x-4" onSubmit={this.handleSubmit}>
+                <label class="text-xl font-medium text-black">Title: </label>
+                <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent rounded shadow-md" type="text" value={this.state.title} onChange={this.handleChange} name="title" />
+                <label class="text-xl font-medium text-black">Content: </label>
+                <textarea class="border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent rounded shadow-md" value={this.state.content} onChange={this.handleChange} name="content" />
+                <input class="bg-purple-700 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 text-xl font-medium text-white rounded-xl shadow-md p-2" type="submit" value="Publish"/>
             </form>
         )
     }
